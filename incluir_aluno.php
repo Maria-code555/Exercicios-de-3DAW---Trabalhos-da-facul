054454baf3b1b07af74f199c9b6fb289c3d56467
Maria Eduarda <santaeduardamaria@gmail.com>
2026-08-22 18:51:04 -0300

    Add files via upload

---
+++ incluir_aluno.php
@@ -0,0 +1,99 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST["nome"];
+    $matricula = $_POST["matricula"];
+    $email = $_POST["email"];
+    $cpf = $_POST["cpf"];
+
+    $msg = "";
+
+    if (!file_exists("alunos.txt")) {
+
+        $arqAluno = fopen("alunos.txt", "w") or die("erro ao criar");
+
+        $linha = "nome;matricula;email;cpf\n";
+
+        fwrite($arqAluno, $linha);
+
+        fclose($arqAluno);
+    }
+
+    $arqAluno = fopen("alunos.txt", "a") or die("erro ao criar");
+
+    $linha = $nome . ";" . $matricula . ";" . $email . ";" . $cpf . "\n";
+
+    fwrite($arqAluno, $linha);
+
+    fclose($arqAluno);
+
+    $msg = "Aluna(o) incluido com sucesso! 💗";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Incluir Aluno</title>
+
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <h1>Incluir Aluno</h1>
+
+        <p class="subtitulo">
+            Preencha os dados do aluno
+        </p>
+
+        <form action="incluir_aluno.php" method="POST">
+
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required>
+
+        <br><br>
+
+        <label for="matricula">Matrícula:</label>
+        <input type="number" name="matricula" required>
+
+        <br><br>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" required>
+
+        <br><br>
+
+        <label for="cpf">CPF:</label>
+        <input type="number" name="cpf" required>
+
+        <br><br>
+
+        <input type="submit" value="incluir Aluno">
+
+        </form>
+
+        <?php if (!empty($msg)) { ?>
+
+            <p class="mensagem">
+                <?php echo $msg; ?>
+            </p>
+
+        <?php } ?>
+
+    </div>
+
+</body>
+
+</html>
